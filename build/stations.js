4e7866ab26aa2e47e094865aca99bad5c7239485
Jannis R <mail@jannisr.de>
2017-04-30 20:51:35 +0200

    download stations from API

--- build/stations.js
+++ build/stations.js
@@ -1,34 +1,41 @@
 'use strict'
 
-const pipe = require('multipipe')
-const fs = require('fs')
-const path = require('path')
-const csv = require('csv-parser')
-const map = require('through2-map')
-const cleanKeys = require('./clean-keys')
+// curl -s --fail --header 'Accept: application/json' --header 'Authorization: Bearer '$API_TOKEN 'https://api.deutschebahn.com/stada/v2/stations' -o build/data.json
 
+const qs = require('querystring')
+const {fetch} = require('fetch-ponyfill')()
+const from = require('from2')
 
+const endpoint = 'https://api.deutschebahn.com/stada/v2/stations'
 
-// todo: what is `Kat. Vst`?
-const keys = {
-	  id: (s) => +s['EVA_NR']
-	, ds100: (s) => s['DS100'].toUpperCase()
-	, name: (s) => s['NAME']
-	, latitude: (s) => +s['BREITE']
-	, longitude: (s) => +s['LAENGE']
+const request = (token, offset, size) => {
+	const url = endpoint + '?' + qs.stringify({offset, limit: size})
+	return fetch(url, {
+		headers: {
+			authorization: 'Bearer ' + token,
+			accept: 'application/json'
+		},
+		cache: 'no-store'
+	})
+	.then((res) => {
+		if (!res.ok) throw new Error('non-2xx response')
+		return res.json()
+	})
 }
 
-const parse = (s) => {
-	const r = {}
-	for (let key in keys) r[key] = keys[key](s)
-	return r
+const maxSize = 100
+
+const download = (token) => {
+	let offset = 0
+
+	return from.obj((size, cb) => {
+		size = Math.min(size, maxSize)
+		offset += size
+
+		request(token, offset, size)
+		.then((data) => cb(null, data))
+		.catch(cb)
+	})
 }
 
-
-
-module.exports = pipe(
-	  fs.createReadStream(path.join(__dirname, 'stations.csv'))
-	, csv({separator: ';'})
-	, map.obj(cleanKeys)
-	, map.obj(parse)
-)
+module.exports = download
