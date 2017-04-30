6863ad787d7ef3031ce1b8f62e4e6300dafde7d6
Jannis R <mail@jannisr.de>
2017-04-30 23:15:40 +0200

    build script: prepare parsing

--- build/index.js
+++ build/index.js
@@ -1,27 +1,49 @@
 'use strict'
 
 const pipe = require('multipipe')
-const map = require('through2-map')
+const through = require('through2')
 const ndjson = require('ndjson')
 const fs = require('fs')
 const path = require('path')
 
-const details = require('./details')
 const stations = require('./stations')
+const parseFull = require('./parse-full')
+const parseSimplified = require('./parse-simplified')
 
+const TOKEN = process.env.API_TOKEN
+if (!TOKEN) {
+	process.stderr.write('missing API_TOKEN\n')
+	process.exit(1)
+}
 
+const showError = (err) => {
+	if (!err) return
+	console.error(err)
+	process.exit(1)
+}
 
-details.on('error', console.error)
-.on('data', (details) => {
+const src = pipe(
+	stations(TOKEN),
+	ndjson.parse(),
+	through.obj(function (stations, _, cb) {
+		for (let s of stations) this.push(s)
+		cb()
+	}),
+	showError
+)
 
-	pipe(
+pipe(
+	src,
+	parseSimplified(),
+	ndjson.stringify(),
+	fs.createWriteStream(path.join(__dirname, '../data.ndjson')),
+	showError
+)
 
-		  stations
-		, map.obj((station) => details[station.ds100]
-			? Object.assign({}, details[station.ds100], station)
-			: station)
-		, ndjson.stringify()
-		, fs.createWriteStream(path.join(__dirname, '../data.ndjson'))
-
-	).on('error', console.error)
-})
+pipe(
+	src,
+	parseFull(),
+	ndjson.stringify(),
+	fs.createWriteStream(path.join(__dirname, '../full.ndjson')),
+	showError
+)
