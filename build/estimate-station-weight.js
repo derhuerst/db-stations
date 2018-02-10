4f5faebb729363bca41f373723fe60647cd544e4
Jannis R <mail@jannisr.de>
2018-02-10 18:41:07 +0100

    build: raise request limit to 10/s

--- build/estimate-station-weight.js
+++ build/estimate-station-weight.js
@@ -5,7 +5,7 @@ const createEstimate = require('hafas-estimate-station-weight')
 
 const weights = require('compute-db-station-weight/lib/weights')
 
-const throttledHafas = throttle(5, 1000) // 5 reqs/s
+const throttledHafas = throttle(10, 1000) // 10 reqs/s
 const estimate = createEstimate(throttledHafas, weights)
 
 module.exports = estimate
