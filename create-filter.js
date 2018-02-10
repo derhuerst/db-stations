9cd8a950f2bacc7a81f832edbe09da06f37d1d29
Jannis R <mail@jannisr.de>
2018-02-10 18:34:41 +0100

    create-filter: accept any prop

--- create-filter.js
+++ create-filter.js
@@ -5,24 +5,33 @@ const tokenize = require('tokenize-db-station-name')
 const createFilter = (sel) => {
 	if (sel === 'all') return () => true
 
-	const selTokens = sel.name ? tokenize(sel.name) : []
+	const props = Object.keys(sel)
+	const selTokens = 'string' === typeof sel.name
+		? tokenize(sel.name)
+		: []
 
 	const filter = (s) => {
-		if (('id' in sel) && s.id !== sel.id) return false
-		if (('weight' in sel) && s.weight !== sel.weight) return false
+		for (let i = 0; i < props.length; i++) {
+			const prop = props[i]
 
-		if ('latitude' in sel) {
-			if (!s.location || s.location.latitude !== sel.latitude) return false
-		}
-		if ('longitude' in sel) {
-			if (!s.location || s.location.longitude !== sel.longitude) return false
-		}
-
-		if ('name' in sel) {
-			const sTokens = tokenize(s.name)
-			// check if selTokens is a subset of sTokens
-			for (let i = 0; i < selTokens.length; i++) {
-				if (sTokens.indexOf(selTokens[i]) < 0) return false
+			if (prop === 'name') {
+				const sTokens = tokenize(s.name)
+				// check if selTokens is a subset of sTokens
+				for (let i = 0; i < selTokens.length; i++) {
+					if (sTokens.indexOf(selTokens[i]) < 0) return false
+				}
+			} else if (prop === 'latitude') {
+				if (
+					s.location && sel.location &&
+					s.location.latitude !== sel.location.latitude
+				) return false
+			} else if (prop === 'longitude') {
+				if (
+					s.location && sel.location &&
+					s.location.longitude !== sel.location.longitude
+				) return false
+			} else if (s[prop] !== sel[prop]) {
+				return false
 			}
 		}
 
