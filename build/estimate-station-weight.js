a31109fe31ec8c9c4175b8d2bfbabde60485f518
Jannis R <mail@jannisr.de>
2018-08-18 00:19:14 +0200

    adapt to db-hafas@3 :green_heart:
    closes #21

--- build/estimate-station-weight.js
+++ build/estimate-station-weight.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const throttle = require('db-hafas/throttle')
+const createThrottledHafas = require('db-hafas/throttle')
 const createEstimate = require('hafas-estimate-station-weight')
 
 const weights = require('compute-db-station-weight/lib/weights')
 
-const throttledHafas = throttle(10, 1000) // 10 reqs/s
-const estimate = createEstimate(throttledHafas, weights)
+const hafas = createThrottledHafas('db-stations build', 10, 1000) // 10 reqs/s
+const estimate = createEstimate(hafas, weights)
 
 module.exports = estimate
