723e6b521caf3512b191ac47a2b2a0b09c247072
Jannis R <mail@jannisr.de>
2021-10-21 17:02:55 +0200

    build: estimate station weight with retrying; 4.1.0

--- build/estimate-station-weight.js
+++ build/estimate-station-weight.js
@@ -1,11 +1,17 @@
 'use strict'
 
-const createThrottledHafas = require('db-hafas/throttle')
+const withThrottling = require('hafas-client/throttle')
+const dbProfile = require('hafas-client/p/db')
+const withRetrying = require('hafas-client/retry')
+const createHafasClient = require('hafas-client')
 const createEstimate = require('hafas-estimate-station-weight')
 
 const weights = require('compute-db-station-weight/lib/weights')
 
-const hafas = createThrottledHafas('db-stations build', 15, 1000) // 15 reqs/s
+const throttledDbProfile = withThrottling(dbProfile, 15, 1000) // 15 reqs/s
+const retryingThrottledDbProfile = withRetrying(throttledDbProfile)
+const hafas = createHafasClient(retryingThrottledDbProfile, 'db-stations build')
+
 const estimate = createEstimate(hafas, weights)
 
 module.exports = estimate
